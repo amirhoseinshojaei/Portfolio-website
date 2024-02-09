4e3c3ce528bc699dd7b7139b48d6c0a95f84127f
amirhosein <amirhoseinbu80@gmail.com>
2024-02-09 20:22:18 +0330

    create model for services

--- services/models.py
+++ services/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Service (models.Model):
+
+    title = models.CharField (max_length = 255)
+
+    description = models.TextField (max_length = 2500)
+
+    image = models.ImageField (upload_to='media/service')
+
+    date = models.DateTimeField (auto_now_add = True , auto_now = False)
+
+    user = models.ForeignKey (User,on_delete = models.CASCADE)
+
+    def __str__(self):
+
+        return "{}-{}".format (self.date , self.title)
