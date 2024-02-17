fe6ea8cc6ca68f4f9ae25331d292f9fe0b835415
amirhosein <amirhoseinbu80@gmail.com>
2024-02-17 12:09:33 +0330

    migrate models & register

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+class BlogAdmin (admin.ModelAdmin):
+
+    list_display = [
+        'title',
+        'author',
+        'date',
+        'update_at'
+    ]
+
+    list_filter = [
+        'author',
+        'date',
+        'update_at'
+    ]
+
+admin.site.register (Blog , BlogAdmin)
