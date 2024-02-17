1a40c2c6f29f320ce30f644a621150995ad1f32d
amirhosein <amirhoseinbu80@gmail.com>
2024-02-17 12:09:05 +0330

    create model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog (models.Model):
+
+    title = models.CharField (max_length = 255)
+    description = models.TextField (max_length = 900)
+    img = models.ImageField (upload_to= 'blog' , null= True , blank= True)
+    date = models.DateTimeField (auto_now_add = True , blank = True , null = True , auto_now = False)
+    update_at = models.DateTimeField (auto_now = True , auto_now_add = False ,null = True , blank = True)
+    author = models.ForeignKey (User , on_delete = models.CASCADE)
+
+    def __str__ (self):
+        return "{}-{}".format (self.title , self.date)
+
