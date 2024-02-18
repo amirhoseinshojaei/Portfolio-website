81a48d2dd4c2ab9d712622c90aeeed57057aaa2e
amirhosein <amirhoseinbu80@gmail.com>
2024-02-18 11:26:08 +0330

    create view for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+
+def blog (request):
+
+    blogs = Blog.objects.all()
+    context = {
+        'posts':blogs
+    }
+    return render (request , 'blogs.html', context)
