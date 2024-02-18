fc843ad543b423853a50db881ad79681148d7392
amirhosein <amirhoseinbu80@gmail.com>
2024-02-18 13:08:21 +0330

    create blog-detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Blog
 # Create your views here.
 
@@ -9,3 +9,11 @@ def blog (request):
         'posts':blogs
     }
     return render (request , 'blogs.html', context)
+
+def blog_detail(request,id):
+    
+    blog = get_object_or_404 (Blog , pk = id)
+    context = {
+        'post':blog
+    }
+    return render (request , 'blog-detail.html', context)
