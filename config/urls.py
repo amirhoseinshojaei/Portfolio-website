a8f2c5cd5822a8da957451ba11060bbf24f5305a
amirhosein <amirhoseinbu80@gmail.com>
2024-02-18 11:26:43 +0330

    add path for blog app

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path ("",include('portfolio.urls')),
     path ('authentication/',include('authapp.urls')),
+    path ('blogs/',include ('blog.urls')),
 ]
 
 urlpatterns += static (settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
