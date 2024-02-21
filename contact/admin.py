609719922a39032cc2ba8a43f4561efba7bfe899
amirhosein <amirhoseinbu80@gmail.com>
2024-02-21 21:37:58 +0330

    register model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Contact
 # Register your models here.
+
+class ContactAdmin (admin.ModelAdmin):
+
+    list_display = [
+        'name',
+        'email',
+        'phone',
+        'message'
+    ]
+
+    list_filter = [
+        'email',
+        'name',
+        'phone'
+    ]
+
+admin.site.register (Contact,ContactAdmin)
