48df458049670c3dbfa2822cc973d0f4b7ae5d72
amirhosein <amirhoseinbu80@gmail.com>
2024-02-21 19:59:57 +0330

    create model for contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+class Contact (models.Model):
+
+    name = models.CharField (max_length = 50)
+    email = models.EmailField()
+    phone = PhoneNumberField()
+    message = models.TextField()
+    
