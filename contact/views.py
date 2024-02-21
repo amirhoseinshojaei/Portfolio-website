fbee4fdfc28c88f0b24aa824a2ef84b415434350
amirhosein <amirhoseinbu80@gmail.com>
2024-02-21 21:46:55 +0330

    create view for contact

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from . models import Contact
 # Create your views here.
+
+def contact (request):
+
+    if request.method == "POST":
+
+        fname = request.POST.get ("name")
+        femail = request.POST.get ("email")
+        fphone = request.POST.get ("phone")
+        fmessage = request.POST.get ("message")
+        query = contact (name = fname , email = femail , phone = fphone , message = fmessage)
+        query.save()
+        messages.success (request , "Thanks for contacting us. We will get by you Soon")
+        return redirect ("home")
+    return render (request , 'contact.html')
