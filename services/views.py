25f4f881335f510be2f8978d2efc847cb574d412
amirhosein <amirhoseinbu80@gmail.com>
2024-02-26 22:31:23 +0330

    create service detail view

--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Service
 # Create your views here.
 # TODO ; create view for service and complete app team
@@ -10,3 +10,11 @@ def service_list (request):
         'posts': service
     }
     return render (request , 'service.html', context)
+
+def service_detail (request , id):
+
+    service = get_object_or_404 (Service , pk = id)
+    context = {
+        'post':service
+    }
+    return render (request , 'service_detail.html', context)
