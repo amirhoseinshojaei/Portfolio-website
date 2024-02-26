0207d3d4f8e2b7d9e9dfbe22ffdf46bd7d9780d8
amirhosein <amirhoseinbu80@gmail.com>
2024-02-26 14:03:24 +0330

    create service_list

--- services/views.py
+++ services/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
+# TODO ; create view for service and complete app team
+
+def service_list (request):
+
+    service = Service.objects.all()
+    context = {
+        'posts': service
+    }
+    return render (request , 'service.html', context)
