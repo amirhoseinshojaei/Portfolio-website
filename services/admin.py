8d49bf198739cd1c660f0a8c9b8cf4caa8484ff3
amirhosein <amirhoseinbu80@gmail.com>
2024-02-26 22:34:28 +0330

    register model

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Service
 # Register your models here.
+
+class ServicesAdmin (admin.ModelAdmin):
+
+    list_display = [
+        'title',
+        'date',
+        'user',
+    ]
+
+    list_filter = [
+        'title',
+        'date',
+        'user'
+    ]
+
+    date_hierarchy = 'date'
+
+admin.site.register(Service,ServicesAdmin)
